e4eaa3650e52f6ea33126bdb3cc26ce03876a27e
rifatikkkk <149669085+rifatikkkk@users.noreply.github.com>
2025-02-25 13:57:40 +0300

    (server) get person videos

--- server/src/controllers/user/videoController.ts
+++ server/src/controllers/user/videoController.ts
@@ -28,3 +28,39 @@ export const getVideosByTag = async (req: Request, res: Response) => {
     res.status(500).json({ error: "Internal server error!" });
   }
 };
+
+export const getPersonVideo = async (req: Request, res: Response) => {
+  const { secretCode } = req.body;
+  const { name } = req.params;
+  try {
+    const personVideos = await prisma.person.findUnique({
+      where: {
+        name,
+      },
+      include: {
+        videos: true,
+      },
+    });
+
+    // rewrite code with "if"
+    if (!personVideos) {
+      res.status(404).json({ error: "Person not found!" });
+      return;
+    }
+
+    if (personVideos.secretCode !== secretCode) {
+      res.status(403).json({ error: "Incorrect secret code!" });
+      return;
+    }
+
+    if (personVideos.videos.length === 0) {
+      res.status(204).json({ message: "Person don't have videos!" });
+      return;
+    }
+
+    res.status(200).json(personVideos);
+  } catch (error) {
+    console.error("Error in get Person videos", error);
+    res.status(500).json({ error: "Internal server error!" });
+  }
+};
--- server/src/routes/user/videoRoute.ts
+++ server/src/routes/user/videoRoute.ts
@@ -1,6 +1,10 @@
 import { Router } from "express";
-import { getVideosByTag } from "../../controllers/user/videoController";
+import {
+  getPersonVideo,
+  getVideosByTag,
+} from "../../controllers/user/videoController";
 
 export default (router: Router) => {
   router.get("/video/getTags/:tag", getVideosByTag);
+  router.get("/video/getPerson/:name", getPersonVideo);
 };
